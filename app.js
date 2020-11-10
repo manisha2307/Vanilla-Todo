89ad93bf50c1c93a06e17a372bc60ea7efdb6797
manisha2307 <vallerimanisha2307@gmail.com>
2020-11-10 17:31:41 +0530

    fifth

---
+++ app.js
@@ -0,0 +1,15 @@
+//Select DOM
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+const filterOption = document.querySelector(".filter-todo");
+
+//Event Listeners
+document.addEventListener("DOMContentLoaded", getTodos);
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteTodo);
+filterOption.addEventListener("click", filterTodo);
+
+//Functions
+
+
